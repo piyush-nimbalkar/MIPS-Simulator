c2b6820888db8363a8880fe639957f8129ca2e4e
Piyush Nimbalkar <piyushmnimbalkar@gmail.com>
2014-04-17 23:30:45 -0400

    Replacing 1s and 0s by true and false

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ WORD_SIZE = 4
 MEMORY_BASE_ADDRESS = 256
 INSTRUCTIONS = []
 STAGE = {
-    'IF': 0,
-    'ID': 0,
-    'EX': 0,
-    'WB': 0
+    'IF': False,
+    'ID': False,
+    'EX': False,
+    'WB': False
 }
--- stage.py
+++ stage.py
@@ -10,12 +10,12 @@ class FetchStage(Stage):
         self.instruction = instruction
 
     def run(self, instruction):
-        STAGE['IF'] = 1
+        STAGE['IF'] = True
         print(self.instruction.name + ' in IF')
 
     def next(self):
-        if STAGE['ID'] == 0:
-            STAGE['IF'] = 0
+        if STAGE['ID'] == False:
+            STAGE['IF'] = False
             return DecodeStage(self.instruction)
         return self
 
@@ -25,12 +25,12 @@ class DecodeStage(Stage):
         self.instruction = instruction
 
     def run(self, instruction):
-        STAGE['ID'] = 1
+        STAGE['ID'] = True
         print(instruction.name + ' in ID')
 
     def next(self):
-        if STAGE['EX'] == 0:
-            STAGE['ID'] = 0
+        if STAGE['EX'] == False:
+            STAGE['ID'] = False
             return ExecuteStage(self.instruction)
         return self
 
@@ -41,22 +41,22 @@ class ExecuteStage(Stage):
         self.cycles = 3
 
     def run(self, instruction):
-        STAGE['EX'] = 1
+        STAGE['EX'] = True
         self.cycles -= 1
         print(instruction.name + ' in EX')
 
     def next(self):
-        if self.cycles == 0 and STAGE['WB'] == 0:
-            STAGE['EX'] = 0
+        if self.cycles == 0 and STAGE['WB'] == False:
+            STAGE['EX'] = False
             return executable.Executable.write_back
         return self
 
 
 class WriteBackStage(Stage):
     def run(self, instruction):
-        STAGE['WB'] = 1
+        STAGE['WB'] = True
         print(instruction.name + ' in WB!  \m/')
 
     def next(self):
-        STAGE['WB'] = 0
+        STAGE['WB'] = False
         return None
