96f96c90134f72cc70a6bfd68f4fa39659f9c4f4
Piyush Nimbalkar <piyushmnimbalkar@gmail.com>
2014-04-23 02:41:48 -0400

    Extracting the code in methods for data cache

--- dcache.py
+++ dcache.py
@@ -16,64 +16,82 @@ class DCache:
     @classmethod
     def read(self, address):
         address -= MEMORY_BASE_ADDRESS
-        tag = address >> 5
         blk_no = (address >> 4) % 2
 
         for i in range(CACHE_SETS):
-            if DCache.sets[i].cache_block[blk_no].valid == True and DCache.sets[i].cache_block[blk_no].tag == tag:
-                DCache.lru_for_cache_block[blk_no] = 1 if i == 0 else 0
+            if DCache._is_address_present_in_set(address, i):
+                DCache._set_lru(blk_no, i)
                 return HIT, DCache.sets[i].cache_block[blk_no].words[(address & 12) >> 2]
 
         set_no = DCache.lru_for_cache_block[blk_no]
+
         if DCache.sets[set_no].cache_block[blk_no].dirty:
-            old_tag = DCache.sets[set_no].cache_block[blk_no].tag
-            old_address = (old_tag << 5) | (blk_no << 4)
-            base_address = MEMORY_BASE_ADDRESS + old_address
-            for i in range(CACHE_BLOCK_SIZE):
-                DATA[base_address + (i * WORD_SIZE)] = DCache.sets[set_no].cache_block[blk_no].words[i]
+            DCache._write_back(set_no, blk_no)
 
-        DCache.sets[set_no].cache_block[blk_no].tag = tag
-        DCache.sets[set_no].cache_block[blk_no].valid = True
-        DCache.lru_for_cache_block[blk_no] = 1 if set_no == 0 else 0
-
-        base_address = MEMORY_BASE_ADDRESS + ((address >> 4) << 4)
-        for i in range(CACHE_BLOCK_SIZE):
-            DCache.sets[set_no].cache_block[blk_no].words[i] = DATA[base_address + (i * WORD_SIZE)]
-
+        DCache._setup_block(address, set_no)
         return MISS, DCache.sets[set_no].cache_block[blk_no].words[(address & 12) >> 2]
 
 
     @classmethod
     def write(self, address, value):
         address -= MEMORY_BASE_ADDRESS
-        new_tag = address >> 5
         blk_no = (address >> 4) % 2
 
         for i in range(CACHE_SETS):
-            if DCache.sets[i].cache_block[blk_no].valid == True and DCache.sets[i].cache_block[blk_no].tag == new_tag:
-                DCache.lru_for_cache_block[blk_no] = 1 if i == 0 else 0
-                DCache.sets[i].cache_block[blk_no].dirty = True
-                DCache.sets[i].cache_block[blk_no].words[(address & 12) >> 2] = value
-                return HIT, DCache.sets[i].cache_block[blk_no].words[(address & 12) >> 2]
-
+            if DCache._is_address_present_in_set(address, i):
+                DCache._set_lru(blk_no, i)
+                DCache._set_value(address, i, value)
+                return HIT, value
 
         set_no = DCache.lru_for_cache_block[blk_no]
-        if DCache.sets[set_no].cache_block[blk_no].dirty:
-            old_tag = DCache.sets[set_no].cache_block[blk_no].tag
-            old_address = (old_tag << 5) | (blk_no << 4)
-            base_address = MEMORY_BASE_ADDRESS + old_address
-            for i in range(CACHE_BLOCK_SIZE):
-                DATA[base_address + (i * WORD_SIZE)] = DCache.sets[set_no].cache_block[blk_no].words[i]
 
-        DCache.sets[set_no].cache_block[blk_no].tag = new_tag
+        if DCache.sets[set_no].cache_block[blk_no].dirty:
+            DCache._write_back(set_no, blk_no)
+
+        DCache._setup_block(address, set_no)
+        DCache._set_value(address, set_no, value)
+        return MISS, value
+
+
+    @classmethod
+    def _is_address_present_in_set(self, address, set_no):
+        tag = address >> 5
+        blk_no = (address >> 4) % 2
+        return DCache.sets[set_no].cache_block[blk_no].valid == True and DCache.sets[set_no].cache_block[blk_no].tag == tag
+
+
+    @classmethod
+    def _write_back(self, set_no, blk_no):
+        tag = DCache.sets[set_no].cache_block[blk_no].tag
+        base_address = MEMORY_BASE_ADDRESS + ((tag << 5) | (blk_no << 4))
+        for i in range(CACHE_BLOCK_SIZE):
+            DATA[base_address + (i * WORD_SIZE)] = DCache.sets[set_no].cache_block[blk_no].words[i]
+
+
+    @classmethod
+    def _setup_block(self, address, set_no):
+        blk_no = (address >> 4) % 2
+        DCache.sets[set_no].cache_block[blk_no].tag = address >> 5
         DCache.sets[set_no].cache_block[blk_no].valid = True
+        DCache._set_lru(blk_no, set_no)
+        DCache._read_from_memory(address, set_no)
+
+
+    @classmethod
+    def _set_lru(self, blk_no, set_no):
         DCache.lru_for_cache_block[blk_no] = 1 if set_no == 0 else 0
 
+
+    @classmethod
+    def _read_from_memory(self, address, set_no):
+        blk_no = (address >> 4) % 2
         base_address = MEMORY_BASE_ADDRESS + ((address >> 4) << 4)
         for i in range(CACHE_BLOCK_SIZE):
-            if ((address & 12) >> 2) == i:
-                DCache.sets[set_no].cache_block[blk_no].words[i] = value
-            else:
-                DCache.sets[set_no].cache_block[blk_no].words[i] = DATA[base_address + (i * WORD_SIZE)]
+            DCache.sets[set_no].cache_block[blk_no].words[i] = DATA[base_address + (i * WORD_SIZE)]
 
-        return MISS, DCache.sets[set_no].cache_block[blk_no].words[(address & 12) >> 2]
+
+    @classmethod
+    def _set_value(self, address, set_no, value):
+        blk_no = (address >> 4) % 2
+        DCache.sets[set_no].cache_block[blk_no].dirty = True
+        DCache.sets[set_no].cache_block[blk_no].words[(address & 12) >> 2] = value
